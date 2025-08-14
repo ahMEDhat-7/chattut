46fee9dc974e8dc2dc238c1d79696a92ea93addf
ahmed medhat <am510836@gmail.com>
2025-08-14 17:59:10 +0300

    add: last commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,7 +9,6 @@ import { dbConnect } from "./lib/db.config.js";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import { app, server } from "./lib/socket.js";
-import asyncWrapper from "./middlewares/asyncWrapper.js";
 
 
 const __dirname = path.resolve();
@@ -24,28 +23,22 @@ app.use(
   cors({
     origin: process.env.ORIGIN_URL,
     credentials: true,
+    
   })
 );
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 
-if (process.env.NODE_ENV === "development") {
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
-  
-  app.get("*",asyncWrapper((req, res, next) => {
-      res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));    
-  }))
-}
-app.use((err, req, res, next) => {
-  console.log({
-    status: err.statusText,
-    error: {
-      statusCode: err.statusCode,
-      message: err.message,
-    },
-  });
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(join(__dirname, '../public')));
 
+  app.get('*', (req, res) => {
+    res.sendFile(join(__dirname, '../public/index.html'));
+  });
+}
+
+app.use((err, req, res, next) => {
   res.status(err.statusCode || 500).json({
     status: err.statusText,
     error: {
